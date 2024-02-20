705e4dbf1e8f3422b449ec0ee950077c6adf0654
NhanDoV <60571509+NhanDoV@users.noreply.github.com>
2024-02-20 23:45:48 +0700

    sync

--- Math-test/Set1/my_solution.py
+++ Math-test/Set1/my_solution.py
@@ -2,6 +2,7 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 
+#=============================== Pure mathematics
 def prob_random_triangle(a, b, c):
     # Write your code here
     ma = max(a, b)
@@ -21,7 +22,42 @@ def prob_random_triangle(a, b, c):
     gcdf = math.gcd(denom, numer)
     return f"{denom // gcdf}/{numer // gcdf}"
 
-def determinant_unif_rvs(rank):
-    list_matrices = [np.array([np.random.uniform(0,1,1) for _ in range(rank**2)]).reshape(rank, rank) for _ in range(5000)]
-    list_det = [np.linalg.det(A) for A in list_matrices]
-    return sum(list_det) / 5000
+#================================ Statistics & probability ===================================== #
+# Stats.1
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n*factorial(n-1)
+
+# Stats.2
+def illustration_triangle_from_stick():
+    fig, ax = plt.subplots(1, 2, figsize=(20, 5))
+    ax[0].plot([0, 2, 5, 0], [1, 4, 0, 1], '-')
+    ax[0].plot([0, 2, 5], [1, 4, 0], 'o')
+    ax[0].text(x=1, y=2, s='a')
+    ax[0].text(x=3.2, y=2.5, s='b')
+    ax[0].text(x=3, y=0.5, s='$\ell$ - a - b')
+    ax[0].set_title("Length of stick = $\ell$")
+    ax[1].plot([0, 1, 0, 0], [1, 0, 0, 1], '-')
+    ax[1].plot([0, 1, 0], [1, 0, 0], 'o')
+    ax[1].text(x=-.02, y=.95, s='$\ell$')
+    ax[1].text(x=-.02, y=.5, s='$\ell$ / 2')
+    ax[1].text(y=-.02, x=1.02, s='$\ell$')
+    ax[1].text(y=-.02, x=0.52, s='$\ell$ / 2')
+    ax[1].text(y=0.55, x=0.5, s='x + y = $\ell$')
+    ax[1].text(y=0.25, x=0.2, s='x + y $\geq$ $\ell$ / 2')
+    ax[1].plot([0, 0.5, 0.5, 0], [0.5, 0, 0.5, 0.5], '-')        
+    ax[1].set_title("Illustration")
+    plt.show()
+
+# Stats.3
+def determinant_unif_rvs():
+    N_sample = 1000000
+    EX = 0
+    for k in range(N_sample):
+        EX1 = np.random.rand()
+        EX2 = np.random.rand()
+        EX3 = np.random.rand()
+        EX += EX1*EX3 - EX2**2
+    return EX / N_sample
